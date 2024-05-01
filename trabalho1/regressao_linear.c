cecd0717b47605611736cf7a97b8cdefff8e6c33
nicolediaas <165029412+nicolediaas@users.noreply.github.com>
2024-04-30 22:11:23 -0300

    Add trabalho 1

--- trabalho1/regressao_linear.c
+++ trabalho1/regressao_linear.c
@@ -6,7 +6,6 @@
 int main(int argc, char* argv[]){
 
     FILE *dados;
-
     dados = fopen("dados.csv", "r");
     int x[size];
     float y[size];
@@ -14,24 +13,17 @@ int main(int argc, char* argv[]){
     for(int i = 0; i <= size; i++){
      
      fscanf(dados, "%d,%f", &x[i], &y[i]);
-     printf("%d, %.2f\n", x[i], y[i]);
     }
     
-
     float media_x = 0, media_y = 0;
-    printf("-------------------------------\n");
     for(int j = 0; j <= size; j++){
         
         media_x += (float)x[j];
         media_y += y[j];
-
-        printf("%d, %.2f\n", x[j], media_x);
-
     }
+
     media_x /= size;
     media_y /= size;
-    printf("%.2f, %.2f", media_x, media_y);
-
 
     float inclinacao = 0, baixo, cima;
 
@@ -42,14 +34,11 @@ int main(int argc, char* argv[]){
 
     inclinacao = cima/baixo;
 
-    float intercepcao = 0; 
-
-    intercepcao = media_y - inclinacao * media_x;
+    float intercepcao = media_y - inclinacao * media_x;
 
     printf("y = %.2fx + %.2f", inclinacao, intercepcao);
 
     fclose(dados);
 
     return(0);
-
 }
